ad34b7f368291a72ea3d0c732d47e971ab1403e3
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-22 22:56:41 +0530

    Refactored code to inject dependency and insert values using Value Anotation

--- Spring-value-required-annotation/src/main/java/com/Bridgelabz/value/required/Student.java
+++ Spring-value-required-annotation/src/main/java/com/Bridgelabz/value/required/Student.java
@@ -1,18 +1,23 @@
 package com.Bridgelabz.value.required;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Student  {
 	private String name;
 	private String intrestedCourse;
 	private String hobby;
 	
+	@Value("Priyanka")
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	@Value("Java")
 	public void setIntrestedCourse(String intrestedCourse) {
 		this.intrestedCourse = intrestedCourse;
 	}
-
+	
+	@Value("Lisiting to music")
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
 	}
