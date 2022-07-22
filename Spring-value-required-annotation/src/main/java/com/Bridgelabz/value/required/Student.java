9150906b94c7357e7c7dc757004bf2a085b16936
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-22 23:24:53 +0530

    Refactored code to inject dependency and insert values using Value Anotation before the dependency feild

--- Spring-value-required-annotation/src/main/java/com/Bridgelabz/value/required/Student.java
+++ Spring-value-required-annotation/src/main/java/com/Bridgelabz/value/required/Student.java
@@ -3,25 +3,15 @@ package com.Bridgelabz.value.required;
 import org.springframework.beans.factory.annotation.Value;
 
 public class Student  {
-	private String name;
-	private String intrestedCourse;
-	private String hobby;
-	
 	@Value("${Student.name}")
-	public void setName(String name) {
-		this.name = name;
-	}
+	private String name;
 	
 	@Value("${Student.intrestedCourse}")
-	public void setIntrestedCourse(String intrestedCourse) {
-		this.intrestedCourse = intrestedCourse;
-	}
+	private String intrestedCourse;
 	
 	@Value("${Student.hobby}")
-	public void setHobby(String hobby) {
-		this.hobby = hobby;
-	}
-
+	private String hobby;
+	
 	public void displayStudentInfo() {
 		System.out.println("Student name " + name);
 		System.out.println("Student intrested course " + intrestedCourse);
